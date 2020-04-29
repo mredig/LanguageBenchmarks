36dcc0ad4dc31416f590f776fffd5c9b57f8e031
Michael Redig <mredig@gmail.com>
2020-04-28 20:48:22 -0500

    initial commit

---
+++ LanguageBenchmarks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LanguageBenchmarks
+//
+//  Created by Michael Redig on 4/28/20.
+//  Copyright © 2020 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
